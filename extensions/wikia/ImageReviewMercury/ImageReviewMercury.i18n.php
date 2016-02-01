6f8bd00e39dc173ed8ab0759c676e45962622bef
RafalWilinski <tho.ornee@gmail.com>
2016-02-01 12:58:25 +0100

    QQQs added

--- extensions/wikia/ImageReviewMercury/ImageReviewMercury.i18n.php
+++ extensions/wikia/ImageReviewMercury/ImageReviewMercury.i18n.php
@@ -7,3 +7,14 @@ $messages['en'] = array(
 	'wikia-version-code' => "Wikia code",
 	'wikia-version-config' => "Wikia config"
 );
+
+$messages['qqq'] = array(
+	'imagereview-desc' => '{{desc}}',
+	'imagereview-label-ok' => 'Label tooltip content for option to mark an image as OK.',
+	'imagereview-label-delete' => 'Label tooltip content for option to mark an image for deletion.',
+	'imagereview-label-questionable' => 'Label tooltip content for option to mark an image as questionable.',
+	'imagereview-gotoimage' => 'Tooltip for link to go to image page',
+	'imagereview-option-ok' => 'Text of option to mark an image as OK.',
+	'imagereview-option-delete' => 'Text of option to mark an image for deletion.',
+	'imagereview-option-questionable' => 'Text of option to mark an image as questionable.',
+);
