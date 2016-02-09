1e745b2fdc8c57df613934ee3ebf4fef03a02916
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-02-09 17:02:37 +0100

    finish script + small refactor

--- extensions/wikia/NjordPrototype/maintenance/NjordWikisAdmins.php
+++ extensions/wikia/NjordPrototype/maintenance/NjordWikisAdmins.php
@@ -1,20 +1,57 @@
 <?php
 
-class NjordWikisAdmins extends NjordUsage {
-	public function execute() {
-//		$wikiIds = $this->getWikiIDsWithNjordExt();
-		$db = $this->getDatabaseByCityId(147);
-		$sql = "SELECT user_id FROM user_groups WHERE ug_group = 'sysop'";
-		$res = $db->query($sql);
+require_once( dirname( __FILE__ ) . '/../../../../maintenance/Maintenance.php' );
+require_once( dirname(__FILE__) . '/NjordHelper.php');
 
+
+class NjordWikisAdmins extends Maintenance {
+	public function __construct() {
+		$this->helper = new NjordHelper();
+		parent::__construct();
+	}
+
+	public function execute() {
+		$wikiIds = $this->helper->getWikiIDsWithNjordExt();
+
+		$adminIds = $this -> getUserIds($wikiIds);
+		$admins = $this->getUserData($adminIds);
+		$this->helper->exportToCSV($admins);
+		$this->output("Done\n");
+
+	}
+
+	private function getUserIds($cityIds) {
 		$admins = array();
-		while ($row = $db->fetchObject($res)) {
-			$admins[] = $row->user_name;
+
+		foreach($cityIds as $cityId) {
+			$db = $this->helper->getDatabaseByCityId($cityId);
+			$sql = "SELECT ug_user FROM user_groups WHERE ug_group = 'sysop'";
+			$res = $db->query($sql);
+
+			while ($row = $db->fetchObject($res)) {
+				$admins[] = $row->ug_user;
+			}
 		}
 
-		$this->output(implode('\n', $admins));
+		return array_unique($admins);
+	}
+
+	private function getUserData($userIds) {
+		$db = wfGetDB(DB_SLAVE, array(), 'wikicities');
+		$sql = "SELECT user_name, user_email FROM user WHERE user_id in (".implode(',', $userIds).")";
+		$res = $db->query($sql);
+
+		$userData = array();
+		while ($row = $db->fetchObject($res)) {
+			$userData[] = [
+				'username' => $row->user_name,
+				'email' => $row->user_email
+				];
+		}
+
+		return $userData;
 	}
 }
 
-$maintClass = 'NjordUsage';
+$maintClass = 'NjordWikisAdmins';
 require_once( DO_MAINTENANCE );
