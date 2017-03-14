550059aa2b391d078d4c4d8507213806f363363e
grunny <mwgrunny@gmail.com>
2017-03-14 19:46:45 +1000

    Undo fix for XSS issue in activityfeed tag
    This reverts the fix for IRIS-4090 for the training.

--- extensions/wikia/MyHome/ActivityFeedTag.php
+++ extensions/wikia/MyHome/ActivityFeedTag.php
@@ -16,6 +16,7 @@ function ActivityFeedTag_setup(Parser $parser) {
 }
 
 function ActivityFeedTag_render($content, $attributes, $parser, $frame) {
+	global $wgEnableAchievementsInActivityFeed, $wgEnableAchievementsExt;
 
 	if (!class_exists('ActivityFeedHelper')) {
 		return '';
@@ -24,31 +25,16 @@ function ActivityFeedTag_render($content, $attributes, $parser, $frame) {
 
 	$parameters = ActivityFeedHelper::parseParameters($attributes);
 
-	$element =
-		renderElement( ActivityFeedHelper::getList( $parameters ), getAttributes( $parameters ),
-			getSnippets( $parameters ) );
-	wfProfileOut( __METHOD__ );
-
-	return $element;
-}
-
-function renderElement($content, $attributes, $jsSnippets) {
-	return Html::rawElement( 'div', $attributes, $content ) . $jsSnippets;
-}
-
-function getAttributes( $parameters ) {
-	return empty($parameters['style']) ? [] : ['style' => $parameters['style']];
-}
-
-function getSnippets( &$parameters ) {
-	global $wgEnableAchievementsInActivityFeed, $wgEnableAchievementsExt;
-
+	$tagid = str_replace('.', '_', uniqid('activitytag_', true));	//jQuery might have a problem with . in ID
 	$jsParams = "size={$parameters['maxElements']}";
 	if (!empty($parameters['includeNamespaces'])) $jsParams .= "&ns={$parameters['includeNamespaces']}";
 	if (!empty($parameters['flags'])) $jsParams .= '&flags=' . implode('|', $parameters['flags']);
-
-	$tagid = str_replace('.', '_', uniqid('activitytag_', true));	//jQuery might have a problem with . in ID
 	$parameters['tagid'] = $tagid;
+
+	$feedHTML = ActivityFeedHelper::getList($parameters);
+
+	$style = empty($parameters['style']) ? '' : ' style="' . $parameters['style'] . '"';
+	$timestamp = wfTimestampNow();
 
 	$snippetsDependencies = array('/extensions/wikia/MyHome/ActivityFeedTag.js', '/extensions/wikia/MyHome/ActivityFeedTag.css');
 
@@ -56,15 +42,17 @@ function getSnippets( &$parameters ) {
 		array_push($snippetsDependencies, '/extensions/wikia/AchievementsII/css/achievements_sidebar.css');
 	}
 
-	return JSSnippets::addToStack(
+	$snippets = JSSnippets::addToStack(
 		$snippetsDependencies,
 		null,
 		'ActivityFeedTag.initActivityTag',
 		array(
 			'tagid' => $tagid,
 			'jsParams' => $jsParams,
-			'timestamp' => wfTimestampNow()
+			'timestamp' => $timestamp
 		)
 	);
 
+	wfProfileOut(__METHOD__);
+	return "<div$style>$feedHTML</div>$snippets";
 }
